465738677cad6c005e21d423dc4dd58aa5357232
Yury Rachkouski <6314166@gmail.com>
2015-04-19 21:38:18 +0300

    Task3
    Задание 3. Куча проверок с перепроверками, но работает

--- Mission 0/Task3/Program.cs
+++ Mission 0/Task3/Program.cs
@@ -10,6 +10,22 @@ namespace Task3
     {
         static void Main(string[] args)
         {
-        }
+            Triangle triangle = new Triangle();
+            Console.WriteLine("Тип треугольника со сторонами А={0} B={1} C={2} -- {3}", triangle.A, triangle.B, triangle.C, triangle.ShowTriangleType());
+            
+            triangle.ChangeSides(100, 100, 100);
+            Console.WriteLine("Тип треугольника со сторонами А={0} B={1} C={2} -- {3}", triangle.A, triangle.B, triangle.C, triangle.ShowTriangleType());
+            
+            triangle.ChangeSides(200.00, 173.20508075689, 100.00);
+            Console.WriteLine("Тип треугольника со сторонами А={0} B={1} C={2} -- {3}", triangle.A, triangle.B, triangle.C, triangle.ShowTriangleType());
+            
+            triangle.ChangeSides(50.00, 60.00, 100.00);
+            Console.WriteLine("Тип треугольника со сторонами А={0} B={1} C={2} -- {3}", triangle.A, triangle.B, triangle.C, triangle.ShowTriangleType());
+           
+            triangle.ChangeSides(91.93, 86.60, 100.00);
+            Console.WriteLine("Тип треугольника со сторонами А={0} B={1} C={2} -- {3}", triangle.A, triangle.B, triangle.C, triangle.ShowTriangleType());
+
+            Console.ReadKey();
+         }
     }
 }
--- Mission 0/Task3/Triangle.cs
+++ Mission 0/Task3/Triangle.cs
@@ -16,37 +16,55 @@ namespace Task3
             RightAngled // прямоугольный
         };
 
-        private double _A, _B, _C;
+        private static readonly string[] TriangleTypesNames = {"Не существует", "Остроугольный", "Тупоугольный", "Прямоугольный"};
+        public static int CalculatePrecision = 10; // точность вычисления
+
+        private double _a, _b, _c;
 
         public double A 
         {
-            get { return _A; } 
-            set { _A = value; }
+            get { return _a; } 
+            set { _a = value; }
         }
 
         public double B
         {
-            get { return _B; }
-            set { _B = value; }
+            get { return _b; }
+            set { _b = value; }
         }
 
         public double C
         {
-            get { return _C; }
-            set { _C = value; }
+            get { return _c; }
+            set { _c = value; }
         }
 
-        public bool CanModificate = false; // by default the tiangle can't be modificated
+        public TriangleTypes TriangleType;
+      
+        /// constructors
 
-        public TriangleTypes TriangleType = TriangleTypes.NonValid;
+        public Triangle()
+        {
+            InitTriangleData(0, 0, 0);
+        }
+
+        public Triangle(double a, double b, double c)
+        {
+            InitTriangleData(a, b, c);
+        }
+  
+        public string ShowTriangleType()
+        {
+            return TriangleTypesNames[(int) TriangleType];
+        }
 
         public static bool SortSides(ref double inA, ref double inB, ref double inC)
         {
             if (IsValid(inA, inB, inC))
             {
                 double a = (inA <= inB) && (inA <= inC) ? inA : (inB <= inA) && (inB <= inC) ? inB : inC; // min
-                double b = (inA <= inB) && (inB <= inC) ? inB : (inB <= inA) && (inA <= inC) ? inB : inC; // average
                 double c = (inA >= inB) && (inA >= inC) ? inA : (inB >= inA) && (inB >= inC) ? inB : inC; // max
+                double b = (a < inB) && (inB < c) ? inB : (a < inA) && (inA < c) ? inA : inC; // average
                 inA = a;
                 inB = b;
                 inC = c;
@@ -56,15 +74,15 @@ namespace Task3
             return false;
         }
 
-        public Triangle(double a, double b, double c)
+        private void InitTriangleData(double a, double b, double c)
         {
             A = a;
             B = b;
             C = c;
 
-            TriangleType = (SortSides(ref _A, ref _B, ref _C)) ? GetTriangleTypes(A, B, C) : TriangleTypes.NonValid;
-            CanModificate = TriangleType != TriangleTypes.NonValid;
+            TriangleType = (SortSides(ref _a, ref _b, ref _c)) ? GetTriangleTypes(A, B, C) : TriangleTypes.NonValid; 
         }
+
 
         public static bool IsValid(double a, double b, double c)
         {
@@ -74,11 +92,21 @@ namespace Task3
 
         public static TriangleTypes GetTriangleTypes(double a, double b, double c)
         {
-            if (IsValid(a,b,c))
+            if (!SortSides(ref a, ref b, ref c))
                 return TriangleTypes.NonValid;
-            SortSides(ref a, ref b, ref c);
-            // todo: get Triangle Type
-            return TriangleTypes.AcuteAngled;
+
+            double cosA = (a*a + b*b - c*c)/ (2 * b *c); // нужен 0 (c определенной точностью) -прямоуголный, или знак <0 - тупой, >0 - острый
+            return (Math.Round(Math.Abs(cosA), CalculatePrecision, MidpointRounding.AwayFromZero) == 0) ? TriangleTypes.RightAngled
+                : (cosA < 0) ? TriangleTypes.ObtuseAngled : TriangleTypes.AcuteAngled;
+        }
+
+        public bool ChangeSides(double a, double b, double c)
+        {
+            if (!IsValid(a, b, c)) 
+                return false;
+
+            InitTriangleData(a, b, c);
+            return true;
         }
     }
 }
